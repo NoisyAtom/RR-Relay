bd2bc9f2968dab0bbd2814134871f8acc420b987
Nicholas Herriot <nicholas.herriot@gmail.com>
2022-06-27 21:08:26 +0100

    Adding new tables for RR

--- rr_relays/teams/admin.py
+++ rr_relays/teams/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Race, Runner, Teams
+
+# admin.site.register(Race)
+# admin.site.register(Runner)
+# admin.site.register(Teams)
+
+
+@admin.register(Race)
+class RaceAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Runner)
+class RunnerAdmin(admin.ModelAdmin):
+    list_display = ("number", "last_name", "first_name", "start_time", "end_time")
+    search_fields = ("last_name__startswith",)
+
+
+@admin.register(Teams)
+class TeamsAdmin(admin.ModelAdmin):
+    list_display = ("number", "name", "runners_A", "runners_B", "runners_C", "runners_D", "runners_E", "runners_F")
+
+
